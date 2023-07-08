e29f680e7d169e1b8261444c443fde8bdaa36754
Yunus <yunusozdemir468@gmail.com>
2023-07-08 18:10:11 +0300

    fixes for Program.cs

--- Mobility.Console/Program.cs
+++ Mobility.Console/Program.cs
@@ -1,14 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int[] numbers = { 2, 2, 3, 5, 5, 6, 7 };
+int[] numbers = { 2, 2, 3, 5, 5, 6, 7,1};
 
-for (int i = 0; i < numbers.Length ; i++)
+for (int i = 0; i < numbers.Length; i++)
 {
-    //Select current number
+    //Şu anki sayıyı al
     var lastNumber = numbers[i];
+    //Önceki index'in sıras
+    var beforeNumberIndex = i;
+    //Atlanması gereken index sayısı
+    var skipIndex = i;
     //Counter for the how much numbers repeating on the array
     var counter = 0;
-    for (int j = 0; j < numbers.Length; j++)
+    for (int j = skipIndex; j < numbers.Length; j++)
     {
         var numberX = numbers[j];
         //If numbersX equals to zero skip
@@ -17,7 +21,11 @@ for (int i = 0; i < numbers.Length ; i++)
         //If lastnumber equals to numberX set the numberX and increase counter
         if (lastNumber == numberX)
         {
-            numberX = 0;
+            //Bir önceki sayıyı 0'a set etme
+            numbers[beforeNumberIndex] = 0;
+            //counter eğer 0'dan büyükse numbers[j] değerini 0'a atmalıyız diğer türlü 2,1 gibi istemediğimiz sonuçlar ortaya çıkıyor
+            if (counter > 0)
+                numbers[j] = 0;
             counter++;
         }
     }
