65003042f1212f8a4c876aa71085d138d8d7796b
Yunus <yunusozdemir468@gmail.com>
2023-07-08 17:09:38 +0300

    Fixes for C++ DLL

--- ListOperationsDll/dllmain.cpp
+++ ListOperationsDll/dllmain.cpp
@@ -7,65 +7,10 @@
 #define EXPORTED_METHOD extern "C" __declspec(dllexport)
 
 EXPORTED_METHOD
-extern "C" {
-    EXPORTED_METHOD
-    struct Node {
-        int data;
-        struct Node* next;
-    };
-
-    struct ListOperations {
-        std::list<int> myList;
-    };
-
-    EXPORTED_METHOD
-    ListOperations* CreateListOperations() {
-        return new ListOperations();
-    }
-    EXPORTED_METHOD
-    void DeleteListOperations(ListOperations* listOps) {
-        delete listOps;
-    }
-    EXPORTED_METHOD
-    void AddToList(ListOperations* listOps, int data) {
-        listOps->myList.push_back(data);
-    }
-    EXPORTED_METHOD
-    Node* GetList(ListOperations* listOps) {
-        if (listOps->myList.empty()) {
-            return nullptr;
-        }
-
-        Node* head = nullptr;
-        Node* current = nullptr;
-
-        for (int data : listOps->myList) {
-            Node* newNode = new Node;
-            newNode->data = data;
-            newNode->next = nullptr;
-
-            if (head == nullptr) {
-                head = newNode;
-                current = newNode;
-            }
-            else {
-                current->next = newNode;
-                current = newNode;
-            }
-        }
-
-        return head;
-    }
-    EXPORTED_METHOD
-    void PrintList(Node* head) {
-        Node* current = head;
-        while (current != nullptr) {
-            std::cout << current->data << " ";
-            current = current->next;
-        }
-        std::cout << std::endl;
-    }
+int Multiply(int a, int b) {
+	return a * b;
 }
+
 
 
 
--- Mobility.WorkerService/Worker.cs
+++ Mobility.WorkerService/Worker.cs
@@ -16,47 +16,16 @@ namespace Mobility.WorkerService
         {
             _logger = logger;
         }
-        private const string dllPath = @"C:\\Users\\yunus\\source\\repos\\MobilityPay\\x64\\Debug\ListOperationsDll.dll";
-     
+        private const string dllPath = @"C:\Users\yunus\Documents\GitHub\WinForms-CRUD-EFCore\x64\Debug\ListOperationsDll.dll";
+
         [DllImport(dllPath)]
-        public static extern object PrintList(ListOperations node);
-        [DllImport(dllPath)]
-        public static extern IntPtr CreateListOperations();
-        [DllImport(dllPath)]
-        public static extern ListOperations CreateNode(int data);
-        [DllImport(dllPath)]
-        public static extern void main();
-        [DllImport(dllPath)]
-        public static extern void DeleteListOperations(IntPtr listOps);
-        [DllImport(dllPath)]
-        public static extern void AddToList(IntPtr listOps, int data);
-        [DllImport(dllPath)]
-        public static extern IntPtr GetList(IntPtr listOps);
-        [DllImport(dllPath)]
-        public static extern void PrintList(IntPtr head);
+        public static extern int Multiply(int a, int b);
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            IntPtr listOpsPtr = CreateListOperations();
-
-            AddToList(listOpsPtr, 10);
-            AddToList(listOpsPtr, 20);
-            AddToList(listOpsPtr, 30);
-            var getList = GetList(listOpsPtr);
-            int value = Marshal.ReadInt32(getList);
-
-            IntPtr headPtr = GetList(listOpsPtr);
-            int intValue = Marshal.ReadInt32(headPtr);
-            PrintList(headPtr);
-
-            DeleteListOperations(listOpsPtr);
-            //var asd2 = addToEnd(1);
-            //var asd22 = getList();
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
-            }
+            int result = Multiply(3, 5);
+            _logger.LogInformation(result.ToString());
+            await Task.Delay(1000, stoppingToken);
         }
     }
 }
